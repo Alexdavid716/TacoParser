b2e8045791375842aa0bc0ca99d001fb40898336
Alex David <alexdavid716@gmail.com>
2024-04-01 14:59:26 -0400

    taco parser

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -31,14 +31,28 @@ namespace LoggingKata.Test
             //       represents a TacoBell location
 
             //Arrange
-
+            var tacoParser = new TacoParser();
             //Act
-
+            var actual = tacoParser.Parse(line);
             //Assert
+            Assert.Equal(expected, actual.Location.Longitude);
         }
 
 
         //TODO: Create a test called ShouldParseLatitude
+        [Theory]
+        [InlineData("31.597099,-84.176122,Taco Bell Albany...", 31.597099)]
+        [InlineData("32.571331,-85.499655,Taco Bell Auburn...", 32.571331)]
+        [InlineData("33.49763,-86.875722,Taco Bell Birmingham...", 33.49763)]
 
+        public void ShouldParseLatitude(string line, double expected)
+        {
+            var tacoParser = new TacoParser();
+
+            var actual = tacoParser.Parse(line);
+
+            Assert.Equal(expected, actual.Location.Latitude);
+        }
     }
+
 }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,4 +1,6 @@
-﻿namespace LoggingKata
+﻿using System.Xml.Linq;
+
+namespace LoggingKata
 {
     /// <summary>
     /// Parses a POI file to locate all the Taco Bells
@@ -18,35 +20,58 @@
             if (cells.Length < 3)
             {
                 // Log error message and return null
+                logger.LogError("Error parsing line: Insufficient data");
                 return null; 
             }
 
             // TODO: Grab the latitude from your array at index 0
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
-            
-            
+            double latitude;
+            if (!double.TryParse(cells[0], out latitude))
+            {
+                // Log error message and return null if parsing fails
+                logger.LogError("Error parsing latitude");
+                return null;
+            }
+
+
             // TODO: Grab the longitude from your array at index 1
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
-            
-            
+            double longitude;
+            if (!double.TryParse(cells[1], out longitude))
+            {
+                // Log error message and return null if parsing fails
+                logger.LogError("Error parsing longitude");
+                return null;
+            }
+
+
             // TODO: Grab the name from your array at index 2
-            
+            string name = cells[2];
 
             // TODO: Create a TacoBell class
             // that conforms to ITrackable
-            
+
             // TODO: Create an instance of the Point Struct
             // TODO: Set the values of the point correctly (Latitude and Longitude) 
 
+
+            var location = new Point();
+            location.Latitude = latitude;
+            location.Longitude = longitude;
+
             // TODO: Create an instance of the TacoBell class
+            var tacoBell = new TacoBell();
             // TODO: Set the values of the class correctly (Name and Location)
+            tacoBell.Name = name;
+            tacoBell.Location = location;
 
             // TODO: Then, return the instance of your TacoBell class,
             // since it conforms to ITrackable
 
-            return null;
+            return tacoBell;
         }
     }
 }
